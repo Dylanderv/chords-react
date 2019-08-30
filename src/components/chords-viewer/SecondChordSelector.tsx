64245715ad481464a6e994de444c10fb5de446fb
Dylan Dervaux <dylan.dervaux@orange.com>
2019-08-30 12:24:10 +0200

    wip after second chord to command bar

--- src/components/chords-viewer/SecondChordSelector.tsx
+++ src/components/chords-viewer/SecondChordSelector.tsx
@@ -2,34 +2,32 @@ import React from 'react';
 import useChordService from '../../hooks/useChordService';
 import { InstrumentType } from '../../model/InstrumentType';
 import { getDisplayDataFromMainChordSelector } from '../../utils/chordViewerUtils';
-import { INavLinkGroup } from 'office-ui-fabric-react'
+import { INavLinkGroup, ICommandBarItemProps, CommandBar } from 'office-ui-fabric-react'
 import { RouteComponentProps } from 'react-router';
 import { CHORD_VIEWER_BASE_ROUTE } from '../../utils/routerUtils';
 import FabricNavReactRouter from '../FabricNavReactRouter';
 import { link } from 'fs';
 import { url } from 'inspector';
+import { useReactRouter } from '../../hooks/useReactRouter';
 
 type TParams = {instrument: InstrumentType|'instrumentList', mainChord: string}
 
 
 function SecondChordSelector ({ match }: RouteComponentProps<TParams>) {
   const service = useChordService(match.params.instrument);
+  const { history } = useReactRouter();
   let rawData: string[] = [];
-  let data: INavLinkGroup[] = [];
+  let data: ICommandBarItemProps[] = [];
   if (service.status === "loaded") {
     // Récupérer les données à afficher dans le selector
     rawData = getDisplayDataFromMainChordSelector(service.payload, match.params.mainChord);
 
-    data = [{
-      name: 'chord',
-      links: rawData.map(elem => ({
-        key: elem,
-        name: elem,
-        url: CHORD_VIEWER_BASE_ROUTE + '/' + match.params.instrument + '/' + match.params.mainChord + '/' + elem
-      }))
-    }]
-
-    
+    data = rawData.map(data => ({
+      key: data,
+      name: data,
+      // href: CHORD_VIEWER_BASE_ROUTE + "/" + match.params.instrument + '/' + data,
+      onClick: () => {history.push(CHORD_VIEWER_BASE_ROUTE + "/" + match.params.instrument + '/' + match.params.mainChord + '/' + data)}
+    }))
 
   }
   return (
@@ -37,7 +35,7 @@ function SecondChordSelector ({ match }: RouteComponentProps<TParams>) {
         {service.status === 'loading' && <div>Loading...</div>}
         {service.status === 'loaded' && (
           <div>
-            <FabricNavReactRouter groups={data}></FabricNavReactRouter>
+            <CommandBar items={data}></CommandBar>
           </div>
           
          )}
