2c1e0541a302ee184cdf8cb63c68e8c4962169f2
Dylan Dervaux <dylanderv05@gmail.com>
2019-10-09 22:04:30 +0200

    feat: add chord tooltip on content

--- src/components/partitions/PartitionViewer.tsx
+++ src/components/partitions/PartitionViewer.tsx
@@ -65,6 +65,10 @@ const PartitionViewer: React.FC = () => {
   const { data, loading, error } = useQuery(partitionQueryGetter(match.params.partitionId));
   const auth = useContext(authContext);
   const classes = useStyles();
+
+  if (loading === false && error === undefined) {
+    // displayContent(data.partition.content, data.partition.chords, data.partition.instrument.id, data.partition.instrument.name);
+  }
   
   return (
     <div>
@@ -81,12 +85,12 @@ const PartitionViewer: React.FC = () => {
                 ></ChordViewer>
               }
             >
-              <span>{chord.key + chord.suffix}</span>
+              <Typography variant="body1" display="inline" style={{ fontWeight: "bold" }}>{chord.key + chord.suffix}</Typography>
             </NoBackgroundTooltip>
             
           </div>
           )}
-          <p>{data.partition.content}</p>
+          <div>{displayContent(data.partition.content, data.partition.chords, data.partition.instrument.id, data.partition.instrument.name)}</div>
           {auth.auth.id !== '0' && auth.auth.id === data.partition.owner.id &&
             <Fab component={Link} to={PARTITION_EDITOR_BASE_ROUTE + '/' + data.partition.id} color='secondary' className={classes.fab}><EditIcon/></Fab>
           }
@@ -97,6 +101,47 @@ const PartitionViewer: React.FC = () => {
         )}
       </div>
   )
+}
+
+function displayContent(content: string, chordList: {id: string, key: string, suffix: string}[], instrumentId: string, instrumentName: string) {
+  let contentList = [content]
+  console.log(content);
+  chordList.forEach(chord => contentList = occurencesSplitter(contentList, chord.key + chord.suffix));
+  console.log(contentList);
+  let finalContentList: any[] = [];
+  contentList.forEach((contentElem, pos) => {
+    let chord = chordList.find(chord => (chord.key + chord.suffix) === contentElem);
+    if (chord !== undefined) {
+      finalContentList.push(
+        <NoBackgroundTooltip key={pos} placement="right-start"
+          title={
+            <ChordViewer key={chord.id} isInPartition={true}
+              instrumentId={instrumentId} instrumentName={instrumentName} mainKey={chord.key} suffix={chord.suffix}
+            ></ChordViewer>
+          }
+        >
+          <Typography variant="body1" display="inline" style={{ fontWeight: "bold" }}>{chord.key + chord.suffix}</Typography>
+        </NoBackgroundTooltip>
+      )
+    } else {
+      finalContentList.push(<Typography key={pos} variant="body2" display="inline">{contentElem}</Typography>);
+    }
+  })
+  return finalContentList;
+}
+
+function occurencesSplitter(contentList: string[], elementToFind: string) {
+  let newContentList: string[] = [];
+  contentList.forEach((content: string) => {
+    let splittedContent = content.split(elementToFind);
+    let filledSplittedContent: string[] = [];
+    for (let i = 0; i < splittedContent.length; i++) {
+      filledSplittedContent.push(splittedContent[i]);
+      if (i !== splittedContent.length -1) filledSplittedContent.push(elementToFind);
+    }
+    newContentList = [...newContentList, ...filledSplittedContent];
+  });
+  return newContentList;
 }
 
 export default PartitionViewer;
